50c9dfabd134bcec43ab6b086ec0a70b71efc08d
Michal Mihálik <michal.mhlk@gmail.com>
2025-04-02 14:10:45 +0200

    refactor

--- src/components/behaviors/DragHelpers.ts
+++ src/components/behaviors/DragHelpers.ts
@@ -1,4 +1,6 @@
-import { Mesh, PointerDragBehavior } from "@babylonjs/core";
+import { Mesh, PointerDragBehavior, Quaternion, Vector3 } from "@babylonjs/core";
+import ctx from "../common/SceneContext";
+import meshHelpers from "../common/MeshHelpers";
 
 class DragHelpers {
     removeDragBehavior(mesh: Mesh): void {
@@ -8,6 +10,72 @@ class DragHelpers {
                 mesh.removeBehavior(behavior);
             }
         }
+    }
+
+    arePiecesJoining(meshA: Mesh, meshB: Mesh): boolean {
+        const dataA = ctx.piecesMap.get(meshA)!;
+        const dataB = ctx.piecesMap.get(meshB)!;
+
+        const origRelativePosXZ = new Vector3(
+            dataA.origPos.x - dataB.origPos.x,
+            0,
+            dataA.origPos.z - dataB.origPos.z
+        );
+
+        const currentRelativePosXZ = new Vector3(
+            meshA.getAbsolutePosition().x - meshB.getAbsolutePosition().x,
+            0,
+            meshA.getAbsolutePosition().z - meshB.getAbsolutePosition().z
+        );
+
+        const posEpsilon = 2;
+        const rotEpsilon = 1;
+
+        const identityQuaternion = Quaternion.Identity();
+        const positionMatch = Vector3.Distance(origRelativePosXZ, currentRelativePosXZ) < posEpsilon;
+        const rotationMatch =
+            Quaternion.Dot(meshA.rotationQuaternion!, identityQuaternion) > 1 - rotEpsilon &&
+            Quaternion.Dot(meshB.rotationQuaternion!, identityQuaternion) > 1 - rotEpsilon;
+
+        return positionMatch && rotationMatch;
+    }
+
+    parentUpMeshes(mesh: Mesh, parent: Mesh): Mesh {
+        const neighbourTopParent = meshHelpers.getTopParent(parent);
+
+        if (neighbourTopParent.physicsImpostor) {
+            neighbourTopParent.physicsImpostor.dispose();
+            neighbourTopParent.physicsImpostor = null;
+        }
+
+        mesh.setParent(neighbourTopParent);
+        mesh.rotationQuaternion = Quaternion.Identity();
+        neighbourTopParent.rotationQuaternion = Quaternion.Identity();
+
+        const data = ctx.piecesMap.get(mesh)!;
+        const ntData = ctx.piecesMap.get(neighbourTopParent)!;
+        mesh.position.x = data.origPos.x - ntData.origPos.x;
+        mesh.position.z = data.origPos.z - ntData.origPos.z;
+
+        mesh.computeWorldMatrix(true);
+        mesh.refreshBoundingInfo();
+
+        meshHelpers.makeChildrenSiblings(mesh);
+        dragHelpers.removeDragBehavior(neighbourTopParent);
+
+        return neighbourTopParent;
+    }
+
+    getNeighbours(mesh: Mesh): Mesh[] {
+        const data = ctx.piecesMap.get(mesh)!;
+        const neighbours: Mesh[] = [];
+
+        if (data.xIndex > 0) neighbours.push(ctx.piecesArray[data.xIndex - 1][data.zIndex]);
+        if (data.xIndex < ctx.numX - 1) neighbours.push(ctx.piecesArray[data.xIndex + 1][data.zIndex]);
+        if (data.zIndex > 0) neighbours.push(ctx.piecesArray[data.xIndex][data.zIndex - 1]);
+        if (data.zIndex < ctx.numZ - 1) neighbours.push(ctx.piecesArray[data.xIndex][data.zIndex + 1]);
+
+        return neighbours;
     }
 }
 
--- src/components/behaviors/PieceDragManager.ts
+++ src/components/behaviors/PieceDragManager.ts
@@ -26,62 +26,12 @@ class PieceDragManager {
             draggedNode.animations = [];
     
             if (draggedNode) {
-                const data = ctx.piecesMap.get(draggedNode)!;
-
-                const neighbours: Mesh[] = [];
-                if (data.xIndex > 0) neighbours.push(ctx.piecesArray[data.xIndex - 1][data.zIndex]);
-                if (data.xIndex < ctx.numX - 1) neighbours.push(ctx.piecesArray[data.xIndex + 1][data.zIndex]);
-                if (data.zIndex > 0) neighbours.push(ctx.piecesArray[data.xIndex][data.zIndex - 1]);
-                if (data.zIndex < ctx.numZ - 1) neighbours.push(ctx.piecesArray[data.xIndex][data.zIndex + 1]);
-
-                for (const n of neighbours) {
-                    if (meshHelpers.areMeshesRelated(n, draggedNode)) continue;
-
-                    const nData = ctx.piecesMap.get(n)!;
-                    const origRelativePosXZ = new Vector3(
-                        data.origPos.x - nData.origPos.x,
-                        0,
-                        data.origPos.z - nData.origPos.z
-                    );
-
-                    const currentRelativePosXZ = new Vector3(
-                        draggedNode.getAbsolutePosition().x - n.getAbsolutePosition().x,
-                        0,
-                        draggedNode.getAbsolutePosition().z - n.getAbsolutePosition().z
-                    );
-
-                    const posEpsilon = 2;
-                    const rotEpsilon = 1;
-
-                    const identityQuaternion = Quaternion.Identity();
-                    const positionMatch = Vector3.Distance(origRelativePosXZ, currentRelativePosXZ) < posEpsilon;
-                    const rotationMatch =
-                        Quaternion.Dot(draggedNode.rotationQuaternion!, identityQuaternion) > 1 - rotEpsilon &&
-                        Quaternion.Dot(n.rotationQuaternion!, identityQuaternion) > 1 - rotEpsilon;
-
-                    if (positionMatch && rotationMatch) {
+                for (const n of dragHelpers.getNeighbours(draggedNode)) {
+                    if (dragHelpers.arePiecesJoining(draggedNode, n)) {
                         dragHelpers.removeDragBehavior(draggedNode);
 
-                        const neighbourTopParent = meshHelpers.getTopParent(n);
+                        const neighbourTopParent = dragHelpers.parentUpMeshes(draggedNode, n);
 
-                        if (neighbourTopParent.physicsImpostor) {
-                            neighbourTopParent.physicsImpostor.dispose();
-                            neighbourTopParent.physicsImpostor = null;
-                        }
-
-                        draggedNode.setParent(neighbourTopParent);
-                        draggedNode.rotationQuaternion = identityQuaternion;
-                        neighbourTopParent.rotationQuaternion = identityQuaternion;
-
-                        const ntData = ctx.piecesMap.get(neighbourTopParent)!;
-                        draggedNode.position.x = data.origPos.x - ntData.origPos.x;
-                        draggedNode.position.z = data.origPos.z - ntData.origPos.z;
-
-                        draggedNode.computeWorldMatrix(true);
-                        draggedNode.refreshBoundingInfo();
-
-                        meshHelpers.makeChildrenSiblings(draggedNode);
-                        dragHelpers.removeDragBehavior(neighbourTopParent);
                         let polygon = dragPolygonBuilder.makePolygon(neighbourTopParent);
 
                         const helpBox = MeshBuilder.CreateBox("box", { width: 0.5, height: 0.5, depth: 0.5 }, ctx.scene);
@@ -94,10 +44,11 @@ class PieceDragManager {
                         ctx.polygonMap.set(polygon, helpBox);
 
                         ctx.jigsawPieces.push(polygon);
-                    console.log('new polygon', polygon);
+
                         if (neighbourTopParent.getChildren().length + 1 === ctx.piecesCount) {
                             alert("Job done!");
                         }
+
                         return;
                     }
                 }
