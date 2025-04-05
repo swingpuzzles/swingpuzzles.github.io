7efd047f1bc236ef44cdc108e91e953d9f29b75a
Michal Mihálik <michal.mhlk@gmail.com>
2025-04-05 18:27:50 +0200

    sight range

--- src/components/behaviors/GameModeManager.ts
+++ src/components/behaviors/GameModeManager.ts
@@ -6,7 +6,7 @@ class GameModeManager {
         ctx.camera.beta = 17 * Math.PI / 32;//. = new ArcRotateCamera("arcCamera", Math.PI / 2, 17 * Math.PI / 32, 4 * 45, Vector3.Zero(), scene);
         ctx.camera.attachControl(ctx.canvas, true);
 
-        ctx.camera.upperBetaLimit = 12 * Math.PI / 32;  
+        ctx.camera.upperBetaLimit = 14 * Math.PI / 32;  
         ctx.camera.lowerBetaLimit = 9 * Math.PI / 32;
     }
 
